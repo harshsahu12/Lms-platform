492aae204252702f2e86e80a4ecee98a7098e6e3
harshsahu12 <h693836@gmail.com>
2023-12-02 19:16:19 +0530

    database setup

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,10 @@
+// lib/db.ts
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-};
-
-export const db = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+export const db = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
